9b91a889a1c35d3c381593fdb2d83c30c956fc2a
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-02-23 00:25:19 -0600

    update: readme

---
+++ src/app.js
@@ -0,0 +1,17 @@
+
+// Manipulación de Objetos JSON
+// Autor: FabianXy
+// Fecha: 22 - 02 - 22
+
+const jsonData = require('./users.json');
+
+// Obtención de los No. Telefonicos iterando sobre objeto users:
+const getPhones = (json) => {
+
+    for (let i = 0; i < json.users.length; i++) {
+        console.log(json.users[i].phoneNumber);
+    }
+    return
+}
+
+console.log(getPhones(jsonData));
